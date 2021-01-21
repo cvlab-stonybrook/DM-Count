04015c16139eeefd311d67bf77b5ef30cc1aadb1
Zhaoyi-Yan <yanzhaoyi@outlook.com>
2021-01-21 20:51:14 +0800

    fast create discrete map

--- datasets/crowd.py
+++ datasets/crowd.py
@@ -28,10 +28,20 @@ def gen_discrete_map(im_height, im_width, points):
     num_gt = points.shape[0]
     if num_gt == 0:
         return discrete_map
+    
+    # fast create discrete map
+    points_np = np.array(points).round().astype(int)
+    p_h = np.minimum(points_np[:, 1], np.array([h-1]*num_gt).astype(int))
+    p_w = np.minimum(points_np[:, 0], np.array([w-1]*num_gt).astype(int))
+    p_index = torch.from_numpy(p_h* im_width + p_w)
+    discrete_map = torch.zeros(im_width * im_height).scatter_add_(0, index=p_index, src=torch.ones(im_width*im_height)).view(im_height, im_width).numpy()
+
+    ''' slow method
     for p in points:
         p = np.round(p).astype(int)
         p[0], p[1] = min(h - 1, p[1]), min(w - 1, p[0])
         discrete_map[p[0], p[1]] += 1
+    '''
     assert np.sum(discrete_map) == num_gt
     return discrete_map
 
